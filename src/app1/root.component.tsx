1ec6f06b569c24f0ec804860ebc76ac600b97e0a
Elton <jselton@itcom888.com>
2020-02-03 15:13:03 +0800

    想法验证

--- src/app1/root.component.tsx
+++ src/app1/root.component.tsx
@@ -1,13 +1,29 @@
 import * as React from 'react';
+import {Link, Redirect, Route, Router, Switch} from 'react-router-dom';
+import * as HistoryCreator from 'history';
+
+export const customHistory = HistoryCreator.createBrowserHistory();
 
 export default class Root extends React.Component<any,any> {
   render() {
     const message: string = "This was rendered by app 1 which was written in React";
 
     return (
-        <div style={{marginTop: '100px'}}>
-          {message}
-        </div>
+        <Router history={customHistory}>
+            <div style={{marginTop: '100px'}}>
+                {message}
+                <ul>
+                    <li><Link to={`/app1/react-route`}>React sub route</Link></li>
+                    <li><Link to={`/app2`} >Go app2</Link></li>
+                </ul>
+            </div>
+            <div>
+                <Switch>
+                    <Route path={`/app1/react-route`} render={() => <div>React sub route</div>} />
+                    <Redirect to='/app1' />
+                </Switch>
+            </div>
+        </Router>
     );
   }
 }
--- src/app2/main-module.ts
+++ src/app2/main-module.ts
@@ -1,20 +1,46 @@
-import {NgModule} from '@angular/core';
+import {Component, NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {App2} from './app2.component.ts';
-import {Subroute1} from './subroute1.component.ts';
-import {Subroute2} from './subroute2.component.ts';
 import {enableProdMode} from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {APP_BASE_HREF} from "@angular/common";
 
+
+
+@Component({
+  template: `
+    <div>
+      Ng route is working!
+    </div>
+  `,
+})
+export class NgSubRouteComponent {
+}
+
+@Component({
+  selector: 'app2',
+  template: `
+    <div style="margin-top: 100px;">
+      This was rendered by App2 which is written in Angular
+    </div>
+    <a [routerLink]="['/ng-route']" routerLinkActive="active">Angular route</a>
+    &nbsp;&nbsp;&nbsp;
+    <a (click)="toApp1()" style="color: -webkit-link; cursor: pointer; text-decoration: underline"  >Go App1</a>
+    
+    <div style="height: 1px; margin-top:16px; background: #dfdfdf"></div>
+    
+    <router-outlet></router-outlet>
+  `,
+})
+export class App2Component {
+  toApp1(){
+    (<any>window).singleSpaNavigate("/app1")
+  }
+}
+
 const appRoutes: Routes = [
   {
     path: 'subroute1',
-    component: Subroute1
-  },
-  {
-    path: 'subroute2',
-    component: Subroute2
+    component: NgSubRouteComponent
   },
 ];
 
@@ -27,11 +53,10 @@ enableProdMode();
   ],
   providers: [{provide: APP_BASE_HREF, useValue: '/app2/'}],
   declarations: [
-    App2,
-    Subroute1,
-    Subroute2,
+    App2Component,
+    NgSubRouteComponent,
   ],
-  bootstrap: [App2]
+  bootstrap: [App2Component]
 })
 export default class MainModule {
 }
